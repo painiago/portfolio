9dd5a5330978add70d1d5d1b0b614adb626e37e2
painiago <95057970+painiago@users.noreply.github.com>
2023-07-11 20:40:46 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,8 @@ function App() {
         <link rel="apple-touch-icon" href={favicon} />
         <link rel="icon" href={favicon} />
         <link rel="preconnect" href={linkg}/>
+        <link href="https://fonts.cdnfonts.com/css/the-californication" rel="stylesheet"/>
+                
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
       </Helmet>
 
