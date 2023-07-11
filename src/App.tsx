658c117e4c67ede7e56a6ff5689fe429fa409bc2
painiago <95057970+painiago@users.noreply.github.com>
2023-07-11 11:24:01 -0300

    att favicon

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,27 @@ import SliderLogo from './components/layout/SliderLogos/SliderLogo';
 import Skills from './components/layout/Skills/Skills';
 import Curriculo from './components/curriculo/curriculo';
 import ProjetosDesigner from './components/layout/Projetos/sectiondesigner';
+import { Helmet} from 'react-helmet'
 
+import favicon from '../public/img/iconfav.png'
+const linkg = 'https://fonts.googleapis.com';
 
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#000000" />
+        <title>Iago Nascimento | DevWeb</title>
+        <meta name="description"content="Iago Nascimento | DevWeb | Portifolio"/>
+        <meta http-equiv="Cache-Control" content="public, max-age=86400" />
+        <link rel="apple-touch-icon" href={favicon} />
+        <link rel="icon" href={favicon} />
+        <link rel="preconnect" href={linkg}/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
+      </Helmet>
+
       <NavBar />
       <Welcome />
       <SliderLogo />
