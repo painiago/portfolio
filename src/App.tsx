beba016a030053e238aefe24e5f8c9e6b8931c37
painiago <95057970+painiago@users.noreply.github.com>
2023-07-11 14:02:47 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ function App() {
         <link rel="apple-touch-icon" href={favicon} />
         <link rel="icon" href={favicon} />
         <link rel="preconnect" href={linkg}/>
+        <link rel="stylesheet" href="/public/static/fonts/stylesdesigner.scss"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
       </Helmet>
 
